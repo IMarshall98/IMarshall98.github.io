e4f6e3d36a1454953a692bd6b4a089963ee989bf
IMarshall98 <58204512+IMarshall98@users.noreply.github.com>
2020-03-09 22:10:53 -0400

    js

--- lab_4/scripts/main.js
+++ lab_4/scripts/main.js
@@ -1,2 +1,10 @@
 const myHeading = document.querySelector('.header');
-myHeading.textContent = 'Imaris Lab 4';
+myHeading.textContent = "Imari's Lab 4";
+
+var para1 = document.createElement("P");
+para1.innerText = "My name has # letters";
+document.body.appendChild(para1);
+
+var para2 = document.createElement("P");
+para2.innerText = "The third letter of my first name is #";
+document.body.appendChild(para2);
