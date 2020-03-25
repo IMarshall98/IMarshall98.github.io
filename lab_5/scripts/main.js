52b073a430a548d28945b1ff91a7cc5cc6be793a
IMarshall98 <58204512+IMarshall98@users.noreply.github.com>
2020-03-25 00:07:20 -0400

    try

--- lab_5/scripts/main.js
+++ lab_5/scripts/main.js
@@ -1,2 +1,8 @@
 const myHeading = document.querySelector('.header');
 myHeading.textContent = "Imari's List of Countries";
+
+var ordli = document.createElement("list");
+ordli.innerText = <ol>
+    <li>hi</li>
+</ol>;
+document.body.appendChild(ordli);
