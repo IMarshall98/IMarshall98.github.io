dfdd41dbf19b2bdbe0116c272321de9ae8e89860
IMarshall98 <58204512+IMarshall98@users.noreply.github.com>
2020-05-14 22:17:03 -0400

    continued work lab4

--- lab_4/scripts/main.js
+++ lab_4/scripts/main.js
@@ -1,10 +1,7 @@
-const myHeading = document.querySelector('.header');
-myHeading.textContent = "Imari's Lab 4";
+const newHeader = document.querySelector('.header');
+newHeader.innerHTML= "Imari's Lab 4";
 
-var para1 = document.createElement("P");
-para1.innerText = "My name has # letters";
-document.body.appendChild(para1);
-
-var para2 = document.createElement("P");
-para2.innerText = "The third letter of my first name is #";
-document.body.appendChild(para2);
+var newPara1 = document.createElement("P");
+var Para1 = document.createTextNode("my name has 5  letters");
+newPara1.appendChild(Para1);
+document.getElementsByClassName('.content').appendChild(NewPara1);
