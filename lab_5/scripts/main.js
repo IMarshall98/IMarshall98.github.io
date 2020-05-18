6474fcff16c321f7d9e2091e58164d8d2244e9d2
IMarshall98 <58204512+IMarshall98@users.noreply.github.com>
2020-05-18 16:37:12 -0400

    lab 5 attempt

--- lab_5/scripts/main.js
+++ lab_5/scripts/main.js
@@ -1,12 +1,31 @@
+/*change heading*/
 const myHeading = document.querySelector('.header');
 myHeading.textContent = "Imari's List of Countries";
 
-function addList(){
-    document.querySelector('.content').innerHtml;  put in variable
-    for each country create li element
-    <p>"hi"</p>
+/*ran console.log(countries.length) = 243*/
 
-    docume.create function
-    for each of the 25 countries
-};
+/*inject ordered list into content with class countries*/
+var newList = document.createElement("OL");
+var cont = document.querySelector('.content');
+cont.appendChild(newList);
+newList.className += "countries";
 
+/*onclick function*/
+document.querySelector('.activate').addEventListener("click", function(){
+    const list = document.querySelector('.countries');
+    list.innerHTML = '';
+    for (let i = 0; i < 25; i++){
+        let num = Math.floor(Math.random() * 243);
+        let selected = countries[num];
+        let listItem = document.createElement('li');
+        listItem.innerHTML = JSON.stringify(selected);
+        list.appendChild(listItem);
+    }
+    unselectedCountries = []
+    for (let i = 0; i < countries.length; i++) {
+        let curr = countries[i];
+        if (list.includes(curr) === -1) {
+          let result = curr;
+          unselectedCountries.appendChild(result);
+          console.log(unselectedCountries)}}
+    })
