5ebbe342464f2e61eb710d77f24831b145c94f17
IMarshall98 <58204512+IMarshall98@users.noreply.github.com>
2020-05-18 01:04:22 -0400

    finish lab 4

--- lab_4/scripts/main.js
+++ lab_4/scripts/main.js
@@ -1,7 +1,23 @@
-const newHeader = document.querySelector('.header');
-newHeader.innerHTML= "Imari's Lab 4";
+myName = "Imari Marshall";
+sName = myName.split(" ");
+var firstName = sName[0];
+var lastName = sName[1];
 
-var newPara1 = document.createElement("P");
-var Para1 = document.createTextNode("my name has 5  letters");
-newPara1.appendChild(Para1);
-document.getElementsByClassName('.content').appendChild(NewPara1);
+function clickMe(){
+    const newHeader = document.querySelector('.header');
+    newHeader.innerHTML= "Imari's Lab 4";
+
+    var newPara1 = document.createElement("p");
+    var para1text = document.createTextNode("My name has " + myName.length + " characters");
+    newPara1.appendChild(para1text)
+    var cont = document.querySelector('.content');
+    cont.appendChild(newPara1);
+
+    var newPara2 = document.createElement("p");
+    var para2text = document.createTextNode("The third character of my name is " + firstName.charAt(2).toUpperCase());
+    var para2text2 = document.createTextNode(" " + lastName.substring(lastName.length -3, lastName.length));
+    newPara2.appendChild(para2text);
+    newPara2.appendChild(para2text2);
+    var cont2 = document.querySelector('.content');
+    cont2.appendChild(newPara2);
+}
